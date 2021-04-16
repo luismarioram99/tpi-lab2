aee572567827cda791a34d647f716eeb775fb688
Luis Mario Ramírez Peralta <luismarioram99@gmail.com>
2021-04-15 21:35:06 -0600

    Añadir clases de Warehouse Container y Package
    Creado proyecto Java, asimismo creadas las clases sobre
    las cuales se van a trabajar.

---
+++ laboratorio2/src/container/Package.java
@@ -0,0 +1,14 @@
+package container;
+
+public class Package {
+
+    private double volume = 0;
+
+    public Package(double volume) {
+        this.volume = volume;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+}

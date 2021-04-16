74f4d80eb6404025707bcd38212177650e52e471
Luis Mario Ramírez Peralta <luismarioram99@gmail.com>
2021-04-15 22:47:09 -0600

    Añadir pruebas para container

--- warehouse/test/container/ContainerTest.java
+++ warehouse/test/container/ContainerTest.java
@@ -5,12 +5,12 @@
  */
 package container;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -26,17 +26,18 @@ public class ContainerTest {
     public ContainerTest() {
     }
 
-    @BeforeAll
+    @BeforeClass
     public static void setUpClass() {
     }
 
-    @AfterAll
+    @AfterClass
     public static void tearDownClass() {
     }
 
-    @BeforeEach
+    @Before
     public void setUp() {
-        assertTrue(num_Packages_To_Test > 0, "Test case error, you must test at least 1 Package");
+        assertTrue("Test case error, you must test at least 1 Package",
+                num_Packages_To_Test > 0);
         // We create the Packages that we need.
         b = new Package[num_Packages_To_Test];
         for (int i = 0; i < num_Packages_To_Test; i++) {
@@ -46,7 +47,7 @@ public class ContainerTest {
         container = new Container();
     }
 
-    @AfterEach
+    @After
     public void tearDown() {
     }
 
@@ -58,9 +59,9 @@ public class ContainerTest {
         System.out.println("Add");
         container.clear();
         for (int i = 0; i < num_Packages_To_Test; i++) {
-            assertTrue(container.add(b[i]), "container.add(Package) failed to add a new Package");
-            assertFalse(container.add(b[i]), "container.add(Package) seems to allow the same Package to be added twice");
-            assertTrue(container.contains(b[i]), "container does not contain a Package after it is supposed to have been added");
+            assertTrue("container.add(Package) failed to add a new Package", container.add(b[i]));
+            assertFalse("container.add(Package) seems to allow the same Package to be added twice", container.add(b[i]));
+            assertTrue("container does not contain a Package after it is supposed to have been ", container.contains(b[i]));
         }
     }
 
@@ -70,18 +71,18 @@ public class ContainerTest {
     @Test
     public void testRemove() {
         container.clear();
-        assertFalse(container.remove(b[0]), "container.remove(Package) should fail because Container is empty, but it didn't");
+        assertFalse("container.remove(Package) should fail because Container is empty, but it didn't", container.remove(b[0]));
         for (int i = 0; i < num_Packages_To_Test; i++) {
             container.clear();
             for (int j = 0; j < i; j++) {
                 container.add(b[j]);
             }
             for (int j = 0; j < i; j++) {
-                assertTrue(container.remove(b[j]), "container.remove(Package) failed to remove a Package that is supposed to be inside");
-                assertFalse(container.contains(b[j]), "container still contains a Package after it is supposed to have been removed !");
+                assertTrue("container.remove(Package) failed to remove a Package that is supposed to be inside", container.remove(b[j]));
+                assertFalse("container still contains a Package after it is supposed to have been removed!", container.contains(b[j]));
             }
             for (int j = i; j < num_Packages_To_Test; j++) {
-                assertFalse(container.remove(b[j]), "container.remove(Package) did not fail for a Package that is not inside");
+                assertFalse("container.remove(Package) did not fail for a Package that is not inside", container.remove(b[j]));
             }
         }
     }
@@ -92,12 +93,17 @@ public class ContainerTest {
     @Test
     public void testGetVolume() {
         System.out.println("getVolume");
-        Container instance = new Container();
         double expResult = 0.0;
-        double result = instance.getVolume();
-        assertEquals(expResult, result, 0.0);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        double result = 0.0;
+        for (int i = 0; i < num_Packages_To_Test; i++) {
+            expResult = expResult + b[i].getVolume();
+        }
+        container.clear();
+        for (int i = 0; i < num_Packages_To_Test; i++) {
+            container.add(b[i]);
+        }
+        result = container.getVolume();
+        assertEquals(expResult, result, 0);
     }
 
     /**
@@ -106,12 +112,12 @@ public class ContainerTest {
     @Test
     public void testSize() {
         System.out.println("size");
-        Container instance = new Container();
-        int expResult = 0;
-        int result = instance.size();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        container.clear();
+        for (int i = 0; i < num_Packages_To_Test; i++) {
+            container.add(b[i]);
+        }
+        int result = container.size();
+        assertEquals(num_Packages_To_Test, result);
     }
 
     /**
@@ -120,10 +126,7 @@ public class ContainerTest {
     @Test
     public void testClear() {
         System.out.println("clear");
-        Container instance = new Container();
-        instance.clear();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        container.clear();
     }
 
     /**
@@ -132,13 +135,14 @@ public class ContainerTest {
     @Test
     public void testContains() {
         System.out.println("contains");
-        Package b = null;
-        Container instance = new Container();
-        boolean expResult = false;
-        boolean result = instance.contains(b);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        container.clear();
+        for (int i = 0; i < num_Packages_To_Test; i++) {
+            container.add(b[i]);
+        }
+        for (int i = 0; i < num_Packages_To_Test - 1; i++) {
+            assertTrue("container contains a Package", container.contains(b[i]));
+        }
     }
-
 }
+
+
